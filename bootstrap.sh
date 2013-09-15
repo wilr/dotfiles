74bee4d9b830908ad5e25452056fa1f955378259
Will Rossiter <will.rossiter@gmail.com>
2013-09-15 12:41:49 +1200

    Fix sublime package structure for 3 beta

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,18 +34,18 @@ case $command in
 		for f in `ls -a | grep -P ".{1}[\w][^.]+[\w]$" | grep "[^.git]$" | grep "[^.DS_Store]$"`; do
 			if [ -f ~/$f ]; then
 				result=`diff ~/$f $f`
-				
+
 				if [ "$result" ]; then
-					# It's time for us as a people to start makin' some changes. 
-					# Let's change the way we eat, let's change the way we live 
-					# And let's change the way we treat each other. 
-					# You see the old way wasn't working so it's on us to do 
+					# It's time for us as a people to start makin' some changes.
+					# Let's change the way we eat, let's change the way we live
+					# And let's change the way we treat each other.
+					# You see the old way wasn't working so it's on us to do
 					# What we gotta do, to survive.
 					echo "$(tput setaf 1)$f has changes$(tput sgr0)"
 					echo "$result"
 				else
-					# And still I see no changes can't a brother get a little peace 
-					# It's war on the streets & the war in the Middle East 
+					# And still I see no changes can't a brother get a little peace
+					# It's war on the streets & the war in the Middle East
 					# Instead of war on poverty they got a war on drugs
 					echo "$(tput setaf 2)$f has no no changes$(tput sgr0)"
 				fi
@@ -56,15 +56,22 @@ case $command in
 		;;
 	sublime)
 		# configure sublime 3
-		if [ -d "$HOME/Library/Application Support/Sublime Text 3/Packages" ]; then
-			rsync --exclude ".DS_Store" -av "sublime/" "$HOME/Library/Application Support/Sublime Text 3/Packages"
+		packages="$HOME/Library/Application Support/Sublime Text 3/Installed Packages";
+		user="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+
+		if [ -d "$packages" ]; then
+			rsync --exclude ".DS_Store" -av "sublime/Installed Packages/" "$packages"
+		fi
+
+		if [ -d "$user" ]; then
+			rsync --exclude ".DS_Store" -av "sublime/Packages/User/" "$user"
 		fi
 
 		# configure sublime 2
-		# if [ -d "$HOME/Library/Application Support/Sublime Text 2/Packages" ]; then 
+		# if [ -d "$HOME/Library/Application Support/Sublime Text 2/Packages" ]; then
 		#		rsync --exclude ".DS_Store" -av "sublime/" "$HOME/Library/Application Support/Sublime Text 2/Packages"
 		# fi
-			
+
 		;;
 	*)
 		echo "Unknown command $command provided."
